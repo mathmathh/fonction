6b1b2a7af554a454fde6564d4c38b28689a003b3
mathmathh <145431064+mathmathh@users.noreply.github.com>
2023-09-22 11:51:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function saluer(){
+    console.log("bonjour bienvenue");
+}
+saluer();
+
+let pseudo="ralftou";
+function saluerQqun(prenom){
+    console.log("bonjour"+prenom);
+}
+saluerQqun("bob");
+saluerQqun(speudo);
